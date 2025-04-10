85b202ea545c80710b0d618de1e2e8d335a864cc
SonyaBW <sonyawong@ufl.edu>
2025-04-10 10:45:03 -0400

    corrected the log and div functions

--- calculator.py
+++ calculator.py
@@ -17,10 +17,15 @@ def multiply(a, b):
     a * b
 
 def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
     b / a   # raise ZeroDivisionError if a == 0
 
 def logarithm(a, b):
+    if b <= 0:
+        raise ValueError
     math.log(a,b)# use math library/raise ValueError
+
 
 def exponent(a, b):
     a**b
